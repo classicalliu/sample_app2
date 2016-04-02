35a12417b49b0ed39163f522f6ccc518a6af252b
classicalliu <classicalliu@gmail.com>
2016-04-02 14:45:11 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -53,15 +54,6 @@ class UsersController < ApplicationController
     end
 
     # 事前过滤器
-    
-    # 确保用户已登陆
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = 'Please log in.'
-        redirect_to login_url
-      end
-    end
 
     # 确保是正确的用户
     def correct_user
